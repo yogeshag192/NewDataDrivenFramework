8a4075e27c57809f4fb9337d7ac68663d2d1b303
yogeshag192 <agrawaly@hcl.com>
2017-12-27 11:30:25 +0530

    commit 1 - 27 dec 2017

--- src/com/cil/ui/TestCILPortal.java
+++ src/com/cil/ui/TestCILPortal.java
@@ -1,8 +1,7 @@
 package com.cil.ui;
 
-import java.io.IOException;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
 import com.common.Base;
 import com.common.BaseTest;
 
