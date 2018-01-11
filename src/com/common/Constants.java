b1e1dd6926453649c9a218d566c452a436a298ce
yogeshag192 <agrawaly@hcl.com>
2018-01-11 16:09:13 +0530

    commit 3

--- src/com/common/Constants.java
+++ src/com/common/Constants.java
@@ -2,12 +2,12 @@ package com.common;
 
 public class Constants {
 
-	public static String CONFIGURATION_PATH = System.getProperty("user.dir") +"\\Files\\config.properties";
-	public static String INPUT_DATA_FILE = System.getProperty("user.dir") +"\\Files\\input.properties";
-	public static String WEBELEMENT_FILE = System.getProperty("user.dir") +"\\Files\\webElement.properties";
-	public static String DOWNLOAD_DIRECTORY = System.getProperty("user.dir") + "\\Downloads\\";
-	public static String CHROME_DRIVER_PATH = System.getProperty("user.dir") +"\\Files\\chromedriver.exe";
-	public static String IE_DRIVER_PATH = System.getProperty("user.dir") +"\\Files\\IEDriverServer.exe";
+	public static String CONFIGURATION_PATH = System.getProperty("user.dir") +System.getProperty("file.separator")+"Files"+System.getProperty("file.separator")+"config.properties";
+	public static String INPUT_DATA_FILE = System.getProperty("user.dir")+System.getProperty("file.separator")+"Files"+System.getProperty("file.separator")+"input.properties";
+	public static String WEBELEMENT_FILE = System.getProperty("user.dir") +System.getProperty("file.separator")+"Files"+System.getProperty("file.separator")+"webElement.properties";
+	public static String DOWNLOAD_DIRECTORY = System.getProperty("user.dir") +System.getProperty("file.separator")+ "Downloads";
+	public static String CHROME_DRIVER_PATH = System.getProperty("user.dir") +System.getProperty("file.separator")+"Files"+System.getProperty("file.separator")+"chromedriver.exe";
+	public static String IE_DRIVER_PATH = System.getProperty("user.dir") +System.getProperty("file.separator")+"Files"+System.getProperty("file.separator")+"IEDriverServer.exe";
 	
 	
 }
